3d1ee4ddc39c9e56b5de545649a1e4605b253c03
Hui Li <withlihui@gmail.com>
2016-02-08 22:37:07 +0800

    u9

---
+++ Chapter (9)/9-11.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import pickle as p
+
+__author__ = 'lihui'
+
+# 9-11
+def menu():
+    global bookmarks_list, bookmarks_file
+    bookmarks_file = "booklist.date"
+    try:
+        f = open(bookmarks_file)
+        bookmarks_list = p.load(f)
+        f.close()
+    except:
+        bookmarks_list = {"search": [["google", "www.google.com", "A search website"]]}
+
+    while True:
+
+        print """
+        Please enter operate:
+        1. Look up
+        2. New
+        3. Edit
+        4. Delete
+        Enter the number('.' to quit): """
+
+        choice = raw_input(">")
+        if choice == ".":
+            break
+
+        if choice == "1":
+            look_up()
+        elif choice == "2":
+            add_new()
+        elif choice == "3":
+            edit()
+        elif choice == "4":
+            delete()
+
+        save_file()
+
+
+def look_up():
+    keyword = raw_input("Enter keyword:")
+    for key in bookmarks_list.keys():
+        for bookmark in bookmarks_list[key]:
+
+            for item in (bookmark[0], bookmark[1]):
+                if keyword in item:
+                    print bookmark
+
+
+def add_new():
+    name = raw_input("Enter name:")
+    url = raw_input("Enter url:")
+    description = raw_input("Enter description(optional):")
+    folder = raw_input("Choose a folder(optional):")
+
+    if folder == "":
+        bookmarks_list["untitled"] = [[name,url,description]]
+    elif folder in bookmarks_list:
+        bookmarks_list[folder].append([name,url,description])
+    else:
+        bookmarks_list[folder] = [[name,url,description]]
+
+
+def edit():
+    for key in bookmarks_list:
+        print key
+        for index, item in enumerate(bookmarks_list[key]):
+            print index, item
+
+    folder = raw_input("Enter the folder:")
+    index = int(raw_input("Enter the index:"))
+    bookmarks_list[folder][index][0] = raw_input("Enter the new name:")
+    bookmarks_list[folder][index][1] = raw_input("Enter the new url:")
+    bookmarks_list[folder][index][2] = raw_input("Enter the new description (optional):")
+    newfolder = raw_input("Choose a new folder: (optional):")
+    if newfolder != "":
+        bookmarks_list[newfolder] = [bookmarks_list[folder][index]]
+        del bookmarks_list[folder][index]
+
+
+def delete():
+    for key in bookmarks_list:
+        print key
+        for index, bookmark in enumerate(bookmarks_list[key]):
+            print index, bookmark
+    prompt = """
+    What are you want to delete?
+    1. folder
+    2. bookmark
+    Enter the number:"""
+    choice = raw_input(prompt)
+
+    folder = raw_input("Enter the folder:")
+    if choice == "1":
+        for bookmark in bookmarks_list[folder]:
+            bookmarks_list["untitled"].append(bookmark)
+        del bookmarks_list[folder]
+    else:
+        index = int(raw_input("Enter the index:"))
+        del bookmarks_list[folder][index]
+
+
+def save_file():
+    f = open(bookmarks_file, 'w')
+    p.dump(bookmarks_list, f)
+    f.close()
+
+    html = "booklist.html"
+    f = open(html, 'w')
+    for folder in bookmarks_list.keys():
+        f.write("""<div><h1 style="color:red"><br>"""+folder+"<br></h1></div>")
+        for bookmark in bookmarks_list[folder]:
+            f.write("<hr>")
+            for item in bookmark:
+                f.write(item+"<br>")
+
+    f.close()
+
+    print bookmarks_list
+
+
+if __name__ == "__main__":
+    menu()
--- Chapter (9)/fuctions.py
+++ Chapter (9)/fuctions.py
@@ -70,16 +70,16 @@ def exam_grade():
 
     def rank(score):
 
-        if score in range(90,101):
-          return "A"
-        elif score in range(80,90):
-          return "B"
-        elif score in range(70,80):
-          return "C"
-        elif score in range(60,70):
-          return "D"
+        if score in range(90, 101):
+            return "A"
+        elif score in range(80, 90):
+            return "B"
+        elif score in range(70, 80):
+            return "C"
+        elif score in range(60, 70):
+            return "D"
         elif score < 60:
-          return "F"
+            return "F"
 
     average = sum(score_list) / float(len(score_list))
     rank_list = [rank(score) for score in score_list]
@@ -102,14 +102,80 @@ def file_cmp(file1, file2):
     return len(smaller), 0      # line start at 0
 
 
-# 9-7
+# 9-7 ?
 def ini_file():
     f1 = open("file.ini", 'r')
     for line in f1.readlines():
         print line,
 
 
-# 9-8
+# 9-8 ?
 def module_info():
     modulename = raw_input("Enter module name:")
-    print help(eval(modulename))
+    print help(modulename)
+
+
+# 9-9
+def check_doc():
+    pass
+
+
+# 9-10
+def finance():
+    import pickle as p
+
+    account_file = "account_balance.data"
+    try:
+        f = open(account_file)
+        account_dict = p.load(f)
+    except:
+        account_dict = {"save": 0, "cheque": 0, "market": 0, "regular save": 0}
+
+    account_choice = {1: "save", 2: "cheque", 3: "market", 4: "regular save"}
+    while True:
+        print """
+            Please choose a account:
+            1. save
+            2. cheque
+            3. market
+            4. regular save
+            Enter the number ('.' to quit) :"""
+
+        account = raw_input(">")
+        if account == ".":
+            break
+        account = account_choice[int(account)]
+
+        while True:
+            print """
+                Please choose operate:
+                1. save
+                2. withdraw
+                3. borrow
+                4. loan
+                Enter the number ('.' to quit) :"""
+
+            operate = raw_input(">")
+            if operate == ".":
+                break
+            money = raw_input("Enter the sum:")
+
+            if operate == "1":
+                account_dict[account] += int(money)
+            elif operate in "234":
+                account_dict[account] -= int(money)
+
+            f = open(account_file, 'w')
+            p.dump(account_dict, f)
+            f.close()
+
+    for key in account_dict.keys():
+        print "%s: %s" % (key, account_dict[key])
+
+    return account_dict
+
+
+
+
+
+
