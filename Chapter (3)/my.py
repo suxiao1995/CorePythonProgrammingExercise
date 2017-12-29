a0476f4eb0c8fc29c970db77c84ea3e128791d53
suxiao1995 <34815082+suxiao1995@users.noreply.github.com>
2017-12-29 16:20:53 +0800

    debug 3-12

--- Chapter (3)/my.py
+++ Chapter (3)/my.py
@@ -9,3 +9,45 @@ fobj = open(fname,'r')
 for i in fobj:
   print i.sprit(ls)
 fobj.close()
+
+# 3-12
+#!/usr/bin/env python
+#coding=utf-8
+
+import os
+
+ls = os.linesep
+
+while True:
+    choice = raw_input('''
+    1、write
+    2、read
+    ''')
+    if choice == '1':
+        all = []
+        while True:
+            fname = raw_input("enter filename\n")
+            if not os.path.exists(fname):
+                fobj = open(fname, 'w')
+                while True:
+                    line = raw_input()
+                    if line == '.':
+                        break
+                    else:
+                        all.append(line)
+                break
+            else:
+                print 'already exist!'
+    elif choice == '2':
+        while True:
+            fname = raw_input("enter filename\n")
+            if os.path.exists(fname):
+                fobj = open(fname, 'r')
+                for i in fobj:
+                    print i.strip(ls)
+                fobj.close()
+                break
+            else:
+                print 'no file'
+    else:
+        print 'wrong choice'
